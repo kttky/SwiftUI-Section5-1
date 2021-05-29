890e8b0a43b69b4c54cedcd02295d17815168441
Kttkky <84614763+kttky@users.noreply.github.com>
2021-05-29 13:29:08 +0900

    Initial Commit

---
+++ SwiftUI-Section5-1/SwiftUI_Section5_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Section5_1App.swift
+//  SwiftUI-Section5-1
+//
+//  Created by 加月拓弥 on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Section5_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
